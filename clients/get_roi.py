4a10465b8962738dd3720d1329286df5f22d780f
rdgraham <richard@rdg.cc>
2015-04-13 13:00:59 -0700

    Added small client to get roi

---
+++ clients/get_roi.py
@@ -0,0 +1,8 @@
+import rpyc
+
+conn = rpyc.connect('localhost', 18861, config = {"allow_public_attrs" : True, \
+                                                       "allow_pickle" : True})
+mean = conn.root.roi_stats('manual')['mean']
+conn.close()
+
+print mean
