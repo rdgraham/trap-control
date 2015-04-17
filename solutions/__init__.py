7028c4a70b353ac3d5be6fce03b3594930f32966
rdgraham <richard@rdg.cc>
2015-04-16 22:31:22 -0700

    Implemented recursive checking for solution classes

--- solutions/__init__.py
+++ solutions/__init__.py
@@ -1,19 +1,22 @@
 __all__ = ['hoa', 'hoa2']
 
-
 # finds all solution classes in package and makes them available in list solution_classes
 import pyclbr
 _classes = []
 solution_classes = []
+
+def has_parent(klass, parent_name):
+    "Recursive method to determine if a class inherits at some point from a named class"
+    if klass == '' or klass == 'object': return False
+    if parent_name in [k.name if isinstance(k, pyclbr.Class) else '' for k in klass.super] : return True
+    for k in klass.super:
+        if has_parent(k, parent_name) : return True
+    return False
+
 for module in __all__:
     _classes = _classes + pyclbr.readmodule('solutions.'+module).values()
 for klass in _classes: 
-    try:
-        # TODO : a better job of this
-        if not (klass.super[0].name == 'Solution' or klass.super[0].super[0].name == 'Solution'):
-            continue
-    except AttributeError:
-        continue
+    if not has_parent(klass, 'Solution'): continue
     module = __import__(klass.module, fromlist=[klass.name])
     solution_classes.append( getattr( module , klass.name) ) 
     print 'Found solution class ', klass.name
@@ -22,7 +25,3 @@ def get_from_description(s):
     for c in solution_classes:
         if c.description == s:
             return c
-
-
-
-
